15370090e610a96c522b5376dc0206468870978b
DevEricOfc <84146938+Eric0254@users.noreply.github.com>
2023-10-25 20:16:46 -0300

    atualização para o banco de dados da game raters

--- bancoPI.sql
+++ bancoPI.sql
@@ -1,16 +1,19 @@
 create database sitejogos;
 use sitejogos;
-create table games(
-gameID int primary key auto_increment,
-gameName varchar(150),
-gameDescription text,
-releaseData date);
+CREATE TABLE CAR (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(255) NOT NULL,
+    DESCRICAO TEXT,
+    IMGURL VARCHAR(255)
+);
 
-create table usuario(
-nome varchar(200),
-authorID int primary key auto_increment,
-email varchar(200),
-dataNascimento date);
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    nick VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
 
 create table opinioes(
 reviewID int primary key auto_increment,
